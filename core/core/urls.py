f456a51c2e6eac1b25c764bdf8ca5d62727e154a
moon_coder <69103662+sadeqiali2000@users.noreply.github.com>
2022-11-16 23:39:17 +0330

    swagger added

--- core/core/urls.py
+++ core/core/urls.py
@@ -47,7 +47,9 @@ urlpatterns = [
     path('main/' , include('main.urls')),
     path('accounts/' , include('accounts.urls')),
     path('cart/' , include('cart.urls')),
+    #path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
